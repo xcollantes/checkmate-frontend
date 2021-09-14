c244912df500e3816c09225a51bbc098461fb6be
Xavier Collantes <collantes.xavier@gmail.com>
2021-09-13 21:52:10 -0700

    Add next js

--- components/header.js
+++ components/header.js
@@ -1,10 +1,40 @@
-import Layout from './layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../css/utils.module.css'
+import Link from 'next/link'
 
-export default function Header() {
+
+export default function Header({ children, home }) {
+  constructor(){
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  function Switch(props) {
+    console.log(props.name);
+    let now = new Date();
+    let currentMin = now.getMinutes();
+    console.log("CURRENT MIN: ", currentMin);
+
+    let randInt = Math.round(Math.random() * 10);
+    console.log(randInt);
+    console.log((randInt % 2 == 0));
+    console.log(this.state.);
+
+    let result;
+    let even = false;
+
+    if (randInt % 2 == 0) {
+      result = <p>This is the option for true for {randInt}</p>;
+      even = true;
+    } else {
+      result = <p>This is the option for false for {randInt}</p>;
+    }
+    return result;
+  }
+
   return (
-    <Layout>
-
-    </Layout>
-
-  )
+    <Switch />
+  );
 }
