66dc757cac58eed73efd162dcd6d487aeaf89568
Xavier Collantes <collantes.xavier@gmail.com>
2021-09-25 19:01:59 -0700

    Add action button on home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import FloatingActionButtons from '../components/actionButton'
 import BaseLayout from '../components/base'
 import utilStyles from '../css/utils.module.css'
+import baseStyles from '../css/base.module.css'
 import configData from '../config.json'
 
 
@@ -9,19 +11,11 @@ export default function Home() {
   const name = "Xavier"
   return (
     <BaseLayout home>
-      <h1 className={utilStyles.heading2Xl}>{configData.METADATA.WEBSITE_NAME}</h1>
-      <p>{configData.METADATA.WEBSITE_DESCRIPTION}</p>
-      <p>TYLER GETS ME a job as a waiter, after that Tyler's pushing a gun in my mouth and saying, the
-first step to eternal life is you have to die. For a long time though,Tyler and I were best friends.
-People are always asking, did I know about Tyler Durden.
-
-The barrel of the gun pressed against the back of my throat,Tyler says "We really won't die."
-
-With my tongue I can feel the silencer holes we drilled into the barrel of the gun. Most of the
-noise a gunshot makes is expanding gases, and there's the tiny sonic boom a bullet makes
-because it travels so fast. To make a silencer, you just drill holes in the barrel of the gun, a lot
-of holes. This lets the gas escape and slows the bullet to below the speed of sound.</p>
-
+    <div className={baseStyles.container}>
+      <h1 className={utilStyles.heading3Xl}>{configData.METADATA.WEBSITE_NAME}</h1>
+      <p className={utilStyles.headingLg}>{configData.METADATA.TAGLINE}</p>
+      <FloatingActionButtons text="Get notified"></FloatingActionButtons>
+    </div>
     </BaseLayout>
   )
 }
