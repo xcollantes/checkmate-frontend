404d0d6936e6542f9b0a89d8f57ba9367e448d03
Xavier Collantes <collantes.xavier@gmail.com>
2021-09-29 17:46:46 -0700

    Add getstatic

--- components/catalogGrid.js
+++ components/catalogGrid.js
@@ -6,15 +6,29 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography'
 import CatalogCard from '../components/catalogCard'
 
+/*
+getStaticProps will never send data on the client.
+*/
+export async function getStaticProps() {
+  let url = "https://api.weatherapi.com/v1/current.json?q=Seattle&key=2d796238cb9b4f0f93700711213009";
+  const response = await fetch(url);
+  const xxx = await response.json();
 
-export default function CatalogGrid( {children} ) {
+  return {
+    props: {
+      xxx
+    }
+  }
+}
+
+function CatalogGrid( {xxx} ) {
   // const rowContent = ()
 
   let resultRow = [];
   for (let row = 0; row <= 100; row++) {
     resultRow.push(
       <Grid item xs={12} sm={6} md={3}>
-        <CatalogCard title="hello"
+        <CatalogCard title={xxx}
                      action="See more">
         </CatalogCard>
       </Grid>
@@ -24,8 +38,10 @@ export default function CatalogGrid( {children} ) {
   return (
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          {resultRow}{children}
+          {resultRow}
         </Grid>
       </Box>
   )
 }
+
+export default CatalogGrid
