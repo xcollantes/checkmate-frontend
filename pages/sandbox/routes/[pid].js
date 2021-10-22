ce0b601818be183725257fdb81f2363106bf8fb8
Xavier Collantes <collantes.xavier@gmail.com>
2021-10-22 16:04:10 -0700

    Test links

--- pages/sandbox/routes/[pid].js
+++ pages/sandbox/routes/[pid].js
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router'
+import Image from 'next/image'
+import Link from 'next/link'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
@@ -7,13 +9,28 @@ import data from '../../../testdata/products.json'
 export default function Template() {
   const router = useRouter()
   const { pid } = router.query
+  console.log(pid)
   console.log(router.query)
+  let x = ""
+  const m = 2
+
+  if (pid == m) {
+    x = <Typography>{pid} is equal to {m}</Typography>
+  }
+
+  const p = data.filter(product => product._id == pid)[0]  // Choose one result
+  console.log(p)
 
   return (
     <Box>
       <Typography variant="h1">Template page</Typography>
       <Typography variant="body1">page id: {pid}</Typography>
       <Typography variant="body1">args: {router.query.argone}</Typography>
+      <Box sx={{ position: "relative", height: "20rem"}}>
+        <Image src={"/../public/images/products/" + p.image}
+               layout="fill"
+               objectFit="contain" />
+      </Box>
     </Box>
   )
 }
