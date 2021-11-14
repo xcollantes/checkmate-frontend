860c62d30f255e0a5ee2d3db45f959c663e71cce
Xavier Collantes <collantes.xavier@gmail.com>
2021-11-13 17:29:35 -0800

    Add custom context for Next

---
+++ context/useFirebaseAuth.js
@@ -0,0 +1,42 @@
+/** Custom context for authentication of users.
+ *
+ *  Modeled after
+ *  https://blog.logrocket.com/implementing-authentication-in-next-js-with-firebase/
+ */
+
+import { useState, useEffect } from 'react'
+import firebase from '/firebase';
+
+const formatAuthUser = (user) => ({
+  uid: user.uid,
+  email: user.email
+});
+
+export default function useFirebaseAuth() {
+  const [authUser, setAuthUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authStateChanged = async (authState) => {
+    if (!authState) {
+      setAuthUser(null)
+      setLoading(false)
+      return;
+    }
+
+    setLoading(true)
+    var formattedUser = formatAuthUser(authState);
+    setAuthUser(formattedUser);
+    setLoading(false);
+  };
+
+  // listen for Firebase state change
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(authStateChanged);
+    return () => unsubscribe();
+  }, []);
+
+  return {
+    authUser,
+    loading
+  };
+}
