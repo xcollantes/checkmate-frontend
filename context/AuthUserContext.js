38e48bcc0db7a8205d6d79569984373952a97d30
Xavier Collantes <collantes.xavier@gmail.com>
2021-11-14 21:46:55 -0800

    Add firebase init files

--- context/AuthUserContext.js
+++ context/AuthUserContext.js
@@ -1,24 +1,39 @@
 import { createContext, useContext, Context } from 'react'
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth"
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword } from "firebase/auth"
 import firebaseAuth from "../firebase"
 
+class AuthUser {
+  const loggingWithEmail = (email, password) => {
+    signInWithEmailAndPassword(firebaseAuth, email, password).then(cred => {
+      const token = cred.accessToken
+      const user = cred.user
+      console.log("GOOGEL AUTH PROVIDER")
+      console.log({cred, token, user})
+    }).catch(e => {
+      const errorMessage = "Error with Google Authentication " + e.code + " " + e.message + " with email " + e.email + " " + GoogleAuthProvider.credentialFromError(e)
+      console.error(errorMessage)
+    })
+  }
 
-const provider = new GoogleAuthProvider()
+  // export const login = () => {
+  //   signInWithPopup(firebaseAuth, provider).then(result => {
+  //     const cred = GoogleAuthProvider.credentialFromResult(result)
+  //     const token = cred.accessToken
+  //     const user = result.user
+  //     console.log("GOOGEL AUTH PROVIDER")
+  //     console.log({cred, token, user})
+  //   }).catch(e => {
+  //     const errorMessage = "Error with Google Authentication " + e.code + " " + e.message + " with email " + e.email + " " + GoogleAuthProvider.credentialFromError(e)
+  //     console.error(errorMessage)
+  //   })
+  // }
 
-const login = () => {
-  signInWithPopup(firebaseAuth, provider).then(result => {
-    const cred = GoogleAuthProvider.credentialFromResult(result)
-    const token = cred.accessToken
-    const user = result.user
-    console.log("GOOGEL AUTH PROVIDER")
-    console.log({cred, token, user})
-  }).catch(e => {
-    const errorMessage = "Error with Google Authentication " + e.code + " " + e.message + " with email " + e.email + " " + GoogleAuthProvider.credentialFromError(e)
-    console.error(errorMessage)
-  })
-}
-
-const logout = () => {
-  firebaseAuth.signOut()
-  console.log("LOGOUT SUCCESSFUL")
+  const logout = () => {
+    firebaseAuth.signOut()
+    console.log("LOGOUT SUCCESSFUL")
+  }
 }
--- pages/sandbox/login/loggedin.js
+++ pages/sandbox/login/loggedin.js
@@ -1,16 +1,42 @@
 import { useEffect } from 'react'
 import useRouter from 'next/router'
 import Box from '@mui/material/Box'
-import useAuth from '../context/AuthUserContext'
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signOut,
+  signInWithPopup,
+  signInWithEmailAndPassword } from "firebase/auth"
+import firebaseAuth from "../../../firebase"
 
+// import { login, loggingWithEmail, logout } from '../../../context/AuthUserContext.js'
+// import SomePrint from '../../../context/AuthUserContext.js'
 
-const { login, logout } = useAuth()
 
 export default function LoggedIn() {
+  const loggingWithEmail = (email, password) => {
+    signInWithEmailAndPassword(firebaseAuth, email, password).then(cred => {
+      const token = cred.accessToken
+      const user = cred.user
+      console.log("GOOGEL AUTH PROVIDER")
+      console.log({cred, token, user})
+    }).catch(e => {
+      const errorMessage = "Error with Google Authentication " + e.code + " " + e.message + " with email " + e.email + " " + GoogleAuthProvider.credentialFromError(e)
+      console.error(errorMessage)
+    })
+  }
+
+  const logout = () => {
+    signOut(firebaseAuth)
+    console.log("LOGOUT SUCCESSFUL")
+  }
+
   return (
     <Box>
-      <button onClick={login}>LOG IN</button>
-      <button onClick={logout}>LOG OUT</button>
+      <input type="text" id="user"></input>
+      <input type="text" id="pass"></input>
+      <button onClick={loggingWithEmail("", "")}>LOG IN</button>
+      <button onClick={logout()}>LOG OUT</button>
     </Box>
   )
 }
