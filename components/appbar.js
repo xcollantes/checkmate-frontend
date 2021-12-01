a7d969363e4533b03c8617dbe7a0a5cb4aeb2158
Xavier Collantes <collantes.xavier@gmail.com>
2021-11-30 20:06:24 -0800

    Fix bug where Login button has background

--- components/appbar.js
+++ components/appbar.js
@@ -1,18 +1,16 @@
 import Link from 'next/link'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
 import utilStyles from '../css/utils.module.css'
 import configData from '../config.json'
 
 
 export default function TopAppBar({ hideLogo, hideLogin }) {
-
-  console.log("HIDELOGIN: ", hideLogin)
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -33,9 +31,16 @@ export default function TopAppBar({ hideLogo, hideLogin }) {
             </Typography>)
           }
 
-          {!hideLogin && <Button color="inherit"><Link href="/login"><a>Login</a></Link></Button>}
+          {!hideLogin && (
+            <Link href="/login">
+              <Button style={{ backgroundColor: "transparent" }}
+                      color="inherit">
+                Login
+              </Button>
+            </Link>)
+          }
         </Toolbar>
       </AppBar>
     </Box>
-  );
+  )
 }
