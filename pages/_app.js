cad5261716897b82766f07dde782c01ea4706885
Xavier Collantes <collantes.xavier@gmail.com>
2021-12-05 18:39:07 -0800

    Add restricted pages with protected

--- pages/_app.js
+++ pages/_app.js
@@ -2,28 +2,53 @@
 // https://nextjs.org/learn/basics/assets-metadata-css/global-styles
 
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import global from '../css/global.css'
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { firebaseApp, firebaseAuth } from '../firebaseApp'
-import { UserContext } from '../components/user'
+import { UserContext, useUserContext } from '../components/user'
 
 
 // Overrides default Material UI: https://mui.com/customization/default-theme
 const theme = createTheme({});
 
 export default function App({ Component, pageProps }) {
+  // const [currentUser, setCurrentUser] = useState()
+  const route = useRouter()
   const [user, loading, error] = useAuthState(firebaseAuth)
 
-  return (
 
+  // Specify `?debug_info=true` for info at bottom of page
+  let urlParamDebug = route.query.deb
+  let debugInfo = true ? urlParamDebug === "true" : false
+
+  // useEffect(() => {
+  //   setCurrentUser(user)
+  // }, [])
+
+  if (pageProps.protected && !user) {
+    return (
+      <iframe width="100%" height="500" src="https://www.youtube.com/embed/RfiQYRn7fBg?controls=0&amp;start=13" title="YouTube video player" frameborder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture" allowfullscreen></iframe>
+    )
+  }
+
+  if (pageProps.protected && loading) {
+    return <>THIS IS PROTECTED PAGE LOADING</>
+  }
+
+  return (
     <UserContext.Provider value={user}>
       <ThemeProvider theme={theme}>
 
         <Component {...pageProps} />
 
-        <h3>DEBUG INFO</h3>
-        {user && <p>USER: {user.displayName}</p>}
+        {(user && debugInfo) &&
+          <div>
+            <h3>DEBUG INFO</h3>
+            <p>USER: {user.displayName}</p>
+          </div>
+        }
 
       </ThemeProvider>
     </UserContext.Provider>
