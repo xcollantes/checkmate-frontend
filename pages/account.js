02c3f767b24bcb99307f60e5e71ca0f4b637c302
Xavier Collantes <collantes.xavier@gmail.com>
2021-12-05 22:29:30 -0800

    Add user account page

---
+++ pages/account.js
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import Link from 'next/link'
+import Box from '@mui/material/Box'
+import Fab from '@mui/material/Fab'
+import Typography from '@mui/material/Typography'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { firebaseAuth } from '../firebaseApp'
+import { useUserContext } from '../contexts/user'
+import BaseLayout from '../components/base'
+
+
+export async function getStaticProps(context) {
+  return {
+    props: { protected: true }
+  }
+}
+
+export default function UserAccount() {
+  const user = useUserContext()
+
+  return (
+    <>
+    {user &&
+      <BaseLayout userAccount>
+        <Box sx={{ mt: "3rem" }}>
+          <Typography variant="h2">
+            Welcome {user.displayName}!
+          </Typography>
+        </Box>
+      </BaseLayout>
+    }
+    </>
+  )
+}
