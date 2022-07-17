364606fe050ed36cb8e3e2f4194434056a51b7af
Xavier Collantes <collantes.xavier@gmail.com>
2022-07-16 20:12:00 -0700

    Add images hosts

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/**
+ * NextJS configuration for web app.
+ */
+
+
+/**
+ * Listing of domains allows for calls to external source.
+ */
+module.exports = {
+    images: {
+        domains: [
+            "storage.cloud.google.com",
+            "storage.googleapis.com",
+            "firebasestorage.googleapis.com"
+        ]
+    }
+}
